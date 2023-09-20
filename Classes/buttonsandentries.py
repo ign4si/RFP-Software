fc8e6173e25854d9ef0eec2cdd31ab21949e7e38
ign4si <lobato31415@gmail.com>
2023-09-20 13:36:43 +0200

    working on buttonsandentries

--- Classes/buttonsandentries.py
+++ Classes/buttonsandentries.py
@@ -87,15 +87,7 @@ class ButtonsAndEntries:
     #         self.entry_list[0].configure(text="On")
     #     if plot==True:
     #         self.canvas.root.master.plot()
-    def switch(self,parameter):
-        if self.button.cget("text")=="On":
-            self.button.configure(text="Off")
-            self.controller.parameters[parameter]=True
-        else:
-            self.button.configure(text="On")
-            self.controller.parameters[parameter]=False
-    def radiobutton(self,parameter):
-        self.controller.parameters[parameter]=self.v.get()
+    
     def fit(self):
         self.canvas.root.master.fit()
     def onepointfit(self):
@@ -112,50 +104,88 @@ class ButtonsAndEntries:
         self.canvas.root.master.color_plot()
 
 class Switchs(ButtonsAndEntries):
-    def __init__(self,canvas,parameter,text,button_width=50,button_height=50,label_width=50,label_height=80,y_button=20):
+    def __init__(self,canvas,controller,parameter,text,button_width=50,button_height=50,label_width=50,label_height=80,y_button=20):
+        super().__init__(controller)
         self.button=tk.Button(canvas.root, text="On", command=lambda: self.switch(parameter))
         canvas.create_window(canvas.posx, canvas.posy+y_button,width=button_width,height=button_height,
                                    window=self.button)
         canvas.create_window(canvas.posx, canvas.posy,width=label_width,height=label_height,
-                                   window=tk.Label(self.canvas.root, text=text,font=NORM_FONT))
+                                   window=tk.Label(canvas.root, text=text,font=NORM_FONT))
+    def switch(self,parameter):
+        if self.button.cget("text")=="On":
+            self.button.configure(text="Off")
+            self.controller.parameters[parameter]=True
+        else:
+            self.button.configure(text="On")
+            self.controller.parameters[parameter]=False
 class RadioButtons(ButtonsAndEntries):
-    def __init__(self,canvas,parameter,possible_values,text,button_width=50,button_height=50,label_width=50,label_height=80,y_button=20):
-        self.v = tk.StringVar(value=canvas.controller.parameters[parameter])
+    def __init__(self,canvas,controller,parameter,possible_values,text,button_width=50,button_height=50,label_width=50,label_height=80,y_button=20):
+        super().__init__(controller)
+        self.v = tk.StringVar(value=self.controller.parameters[parameter])
         self.button_list=[]
         for i in range(len(possible_values)):
             self.button_list.append(tk.Radiobutton(canvas.root,indicatoron=0,
-                                                   text=possible_values[i], variable=self.v, value=possible_values[i],command=lambda: self.radiobutton(parameter)))
+                                                   text=possible_values[i], variable=self.v, value=possible_values[i],command=lambda: self.submit(parameter)))
             canvas.create_window(canvas.posx+i*button_width, canvas.posy+y_button,width=button_width,height=button_height,
                                       window=self.button_list[i])
         canvas.create_window(canvas.posx, canvas.posy,width=label_width,height=label_height,
                                    window=tk.Label(canvas.root, text=text,font=NORM_FONT))
+    def submit(self,parameter):
+        self.controller.parameters[parameter]=self.v.get()
 class Entries(ButtonsAndEntries):
-    def __init__(self,canvas,parameter_list,labels_list,type_list,text,button_width=50,button_height=50,label_width=50,label_height=80,y_button=20):
+    def __init__(self,canvas,controller,parameter_list,labels_list,type_list,button_width=50,button_height=50,label_width=50,label_height=80,y_button=20):
+        super().__init__(controller)
+        self.entry_list=[]
+        for i in range(len(parameter_list)):
+            self.entry_list.append(tk.Entry(canvas.root))
+            canvas.create_window(canvas.posx+i*button_width, canvas.posy+y_button,width=button_width,height=button_height,window=self.entry_list[i])
+            canvas.create_window(canvas.posx+i*button_width, canvas.posy,width=label_width,height=label_height,window=tk.Label(canvas.root, text=labels_list[i],font=NORM_FONT))
+            self.entry_list[i].insert(0, self.controller.parameters[parameter_list[i]])
+        canvas.create_window(canvas.posx+(len(parameter_list)+1)*button_width, canvas.posy+y_button,width=button_width,height=button_height,window=tk.Button(canvas.root, text="Submit", command=lambda: self.submit(parameter_list,type_list)))
+    def submit(self,parameter_list,type_list):
+        for i in range(len(self.entry_list)):
+            self.controller.parameters[parameter_list[i]]=type_list[i](self.entry_list[i].get())
+class Navigator(ButtonsAndEntries):
+    def __init__(self,canvas,controller,parameter,possible_values,text,type=str,button_width=50,button_height=50,label_width=50,label_height=80,entry_width=50,entry_height=50,y_button=20):
+        super().__init__(controller)
         self.entry=tk.Entry(canvas.root)
-        canvas.create_window(canvas.posx, canvas.posy+y_button,width=button_width,height=button_height,
-                                   window=self.entry)
+        canvas.create_window(canvas.posx, canvas.posy+y_button,width=entry_width,height=entry_height,window=self.entry)
+        self.entry.insert(0, self.controller.parameters[parameter])
+
+        #next and previous color in colormap_list buttons
+        canvas.create_window(canvas.posx+entry_width*1.2, canvas.posy+y_button,width=button_width,height=button_height,
+                                   window=tk.Button(canvas.root, text="<", command=lambda: self.previous(parameter,possible_values)))
+        canvas.create_window(canvas.posx+entry_width*1.2+button_width, canvas.posy+y_button,width=button_width,height=button_height,
+                                   window=tk.Button(canvas.root, text=">", command=lambda: self.next(parameter,possible_values)))
+        canvas.create_window(canvas.posx+entry_width*1.2+2*button_width, canvas.posy+y_button,width=button_width,height=button_height,
+                                   window=tk.Button(canvas.root, text="Submit", command=lambda: self.submit(parameter,type)))
         canvas.create_window(canvas.posx, canvas.posy,width=label_width,height=label_height,
                                    window=tk.Label(canvas.root, text=text,font=NORM_FONT))
-    def submit(self,plot=True,load_baseline=False):
-        if self.type=="int":
-            self.canvas.controller.parameters[self.parameter]=int(self.entry.get())
-        if self.type=="float":
-            self.canvas.controller.parameters[self.parameter]=float(self.entry.get())
-        if self.type=="str":
-            self.canvas.controller.parameters[self.parameter]=str(self.entry.get())
-        if self.type=="bool":
-            self.canvas.controller.parameters[self.parameter]=bool(self.entry.get())
-        if self.type=="np.array":
-            self.canvas.controller.parameters[self.parameter]=np.linspace(float(self.entry_list[0].get()),float(self.entry_list[1].get()),int(self.entry_list[2].get()))
-        if self.type=="radiobutton":
-            self.canvas.controller.parameters[self.parameter]=self.v.get()
-        if load_baseline==True:
-            self.canvas.root.master.load_data(reset=False)
-            self.canvas.root.master.load_baseline()
-            self.canvas.root.master.smooth_data()
-        if plot==True:
-            self.canvas.root.master.plot()
+    def previous(self,parameter,possible_values):
+        try:
+            new_value=possible_values[possible_values.index(self.controller.parameters[parameter])-1]
+        except:
+            new_value=possible_values[-1]
+        self.entry.delete(0,tk.END)
+        self.entry.insert(0, new_value)
+    def next(self,parameter,possible_values):
+        try:
+            new_value=possible_values[possible_values.index(self.controller.parameters[parameter])+1]
+        except:
+            new_value=possible_values[0]
+        self.entry.delete(0,tk.END)
+        self.entry.insert(0, new_value)
+    def submit(self,parameter,type):
+        self.controller.parameters[parameter]=type(self.entry.get())
+class FunctionButtons(ButtonsAndEntries):
+    def __init__(self,canvas,controller,func,text,button_width=50,button_height=50):
+        super().__init__(controller)
+        canvas.create_window(canvas.posx, canvas.posy,width=button_width,height=button_height,
+                                   window=tk.Button(canvas.root, text=text, command=lambda: func))
+
+
         
+       
 
 class SweepFile(ButtonsAndEntries):
     def __init__(self,canvas):
